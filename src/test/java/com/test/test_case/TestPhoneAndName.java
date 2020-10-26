bb494da455b76eeebd1b953a46a652c8eca640f2
zhanglin <zhanglin@baicm.com.cn>
2020-10-26 18:57:49 +0800

    测试参数化

--- src/test/java/com/test/test_case/TestPhoneAndName.java
+++ src/test/java/com/test/test_case/TestPhoneAndName.java
@@ -4,14 +4,17 @@ import com.test.data_provider.TestParam;
 import com.test.listener.TestReport;
 import org.testng.Assert;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 @Listeners({TestReport.class})
 public class TestPhoneAndName {
 
+    //    @Test
+//    @Parameters({"demo_1", "demo_2"})
     @Test(description = "获取xls文件中的数据并打印", priority = 1, dataProvider = "getPickUpInfos", dataProviderClass = TestParam.class)
     public void printPhoneAndName(String name, String phone) throws Exception {
-        System.out.println("联系方式\t" + phone);
+        System.out.println(name + "\t联系方式\t" + phone);
         try {
             Long.parseLong(phone);
             new TestLogin().getSms(phone);
