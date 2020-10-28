6b5461a5cb8ecd2069297ac5fb83d5c5e5e1801c
zhanglin <zhanglin@baicm.com.cn>
2020-10-28 22:42:57 +0800

    selenium 隐示等待

--- src/test/java/com/selenium/DriverTest.java
+++ src/test/java/com/selenium/DriverTest.java
@@ -29,36 +29,33 @@ public class DriverTest {
     @Test
     public void runChromeDriver() throws IOException, InterruptedException {
         WebDriver driver = DriverProvider.getChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("http://baicm.yunxuetang.cn/login.htm");
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
         String handle1 = driver.getWindowHandle();
         System.out.println("主页面1句柄：" + handle1);
         driver.findElement(By.id("methodDesktop")).click();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         driver.findElement(By.id("txtUserName2")).sendKeys("05301117");
-        Thread.sleep(500);
+//        Thread.sleep(500);
         driver.findElement(By.id("txtPassword2")).sendKeys("zhanglin0514");
-        Thread.sleep(500);
+//        Thread.sleep(500);
         driver.findElement(By.id("btnSetNoPatt")).click();
-        Thread.sleep(1000);
+//        Thread.sleep(1000);
         driver.findElement(By.linkText("课程资源")).click();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
         driver.findElement(By.id("0fc5e5a3-59eb-42ea-9508-1b993f196383")).click();
-        Thread.sleep(4000);
-
+//        Thread.sleep(4000);
         Set<String> windowHandles = driver.getWindowHandles();
         ArrayList<String> arrayList = new ArrayList<String>(windowHandles);
         arrayList.remove(driver.getWindowHandle());
         driver.close();
         driver.switchTo().window(arrayList.get(0));
-
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         List<WebElement> elements = driver.findElements(By.cssSelector(".font-size-14.hand.text-normal"));
         System.out.println(elements.size());
         elements.get(0).click();
-        Thread.sleep(2000);
+//        Thread.sleep(2000);
         windowHandles = driver.getWindowHandles();
         arrayList = new ArrayList<String>(windowHandles);
         System.out.println(arrayList.size());
