45ec46b788ebe0f5a4dcf2def6bb751323de68ca
zhanglin <zhanglin@baicm.com.cn>
2020-10-29 09:00:09 +0800

    获取新句柄方法.

--- src/test/java/com/selenium/DriverTest.java
+++ src/test/java/com/selenium/DriverTest.java
@@ -17,55 +17,30 @@ public class DriverTest {
     @Test
     public void runFireFoxDriver() throws IOException, InterruptedException {
         WebDriver driver = DriverProvider.getFireDriver();
-
-//        driver.findElement(By.partialLinkText("/kng/kngindex.htm")).click();
-//        Thread.sleep(1000);
-//        System.out.println("77777");
-//        driver.findElement(By.linkText("如何制定产品策略）")).click();
-//        System.out.println("88888");
     }
 
 
     @Test
     public void runChromeDriver() throws IOException, InterruptedException {
+        Set<String> windowHandles;
         WebDriver driver = DriverProvider.getChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);//隐式等待
         driver.get("http://baicm.yunxuetang.cn/login.htm");
-//        Thread.sleep(3000);
         String handle1 = driver.getWindowHandle();
         System.out.println("主页面1句柄：" + handle1);
         driver.findElement(By.id("methodDesktop")).click();
-//        Thread.sleep(2000);
         driver.findElement(By.id("txtUserName2")).sendKeys("05301117");
-//        Thread.sleep(500);
         driver.findElement(By.id("txtPassword2")).sendKeys("zhanglin0514");
-//        Thread.sleep(500);
         driver.findElement(By.id("btnSetNoPatt")).click();
-//        Thread.sleep(1000);
         driver.findElement(By.linkText("课程资源")).click();
-//        Thread.sleep(3000);
+        windowHandles = driver.getWindowHandles();//获取当前handle集合，即将跳转。
         driver.findElement(By.id("0fc5e5a3-59eb-42ea-9508-1b993f196383")).click();
-//        Thread.sleep(4000);
-        Set<String> windowHandles = driver.getWindowHandles();
-        ArrayList<String> arrayList = new ArrayList<String>(windowHandles);
-        arrayList.remove(driver.getWindowHandle());
-        driver.close();
-        driver.switchTo().window(arrayList.get(0));
-//        Thread.sleep(2000);
-        List<WebElement> elements = driver.findElements(By.cssSelector(".font-size-14.hand.text-normal"));
-        System.out.println(elements.size());
-        elements.get(0).click();
-//        Thread.sleep(2000);
+        driver.switchTo().window(DriverProvider.getDiff(windowHandles, driver));//把句柄切到心打开到页面
         windowHandles = driver.getWindowHandles();
-        arrayList = new ArrayList<String>(windowHandles);
-        System.out.println(arrayList.size());
-        arrayList.remove(driver.getWindowHandle());
-        driver.switchTo().window(arrayList.get(0));
-
-
+        List<WebElement> elements = driver.findElements(By.cssSelector(".font-size-14.hand.text-normal"));
+        elements.get(0).click();
+        driver.switchTo().window(DriverProvider.getDiff(windowHandles, driver));
         driver.findElement(By.id("btnStartStudy")).click();
-
-
     }
 
 }
--- src/test/java/com/selenium/util/DriverProvider.java
+++ src/test/java/com/selenium/util/DriverProvider.java
@@ -3,22 +3,46 @@ package com.selenium.util;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import sun.rmi.runtime.Log;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class DriverProvider {
 
     public static WebDriver getFireDriver() throws IOException {
         System.setProperty("webdriver.gecko.driver", new File(".").getCanonicalPath() + "/src/test/resources/driver/geckodriver");
         return new FirefoxDriver();
-
     }
 
 
     public static WebDriver getChromeDriver() throws IOException {
         System.setProperty("webdriver.chrome.driver", new File(".").getCanonicalPath() + "/src/test/resources/driver/chromedriver");
         return new ChromeDriver();
+    }
 
+
+    /**
+     * @param oldSet 跳转新页面之前的Handle集合
+     * @param driver 打开新页面之后，通过driver获取最新的handle集合
+     * @return 通过比较差集，取出新页面的handle
+     */
+    public static String getDiff(Set<String> oldSet, WebDriver driver) {
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String string : oldSet) {
+            if (windowHandles.contains(string)) {
+                windowHandles.remove(string);
+            } else {
+                return string;
+            }
+        }
+
+        if (windowHandles.size() > 0) {
+            return new ArrayList<String>(windowHandles).get(0);
+        }
+
+        return "";
     }
 }
